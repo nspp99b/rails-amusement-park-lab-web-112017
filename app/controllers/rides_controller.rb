9f89a7a3d40c7e38431087d2cc49a8734015fafa
nspp99b <chromepanthers@gmail.com>
2017-12-22 16:42:34 -0500

    take ride allows for compound failure message, action moved from model method to controller

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -7,6 +7,11 @@ class RidesController < ApplicationController
     if @ride.take_ride
       flash[:msg] = @ride.take_ride
     else
+      @user.happiness += @attraction.happiness_rating
+      @user.tickets -= @attraction.tickets
+      @user.nausea += @attraction.nausea_rating
+      byebug
+      @user.save
       flash[:msg] = "Thanks for riding the #{@attraction.name}!"
     end
     redirect_to user_path(@user)
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,16 +3,16 @@ class Ride < ApplicationRecord
   belongs_to :attraction
 
   def take_ride
-
+    answer = "Sorry. "
     if attraction.tickets > user.tickets
-      "You do not have enough tickets the #{attraction.name}."
-    elsif attraction.min_height > user.height
-      "You are not tall enough to ride the #{attraction.name}."
+      answer += "You do not have enough tickets the #{attraction.name}. "
+    end
+    if attraction.min_height > user.height
+      answer += "You are not tall enough to ride the #{attraction.name}."
+    end
+    if answer != "Sorry. "
+      answer
     else
-      user.happiness += attraction.happiness_rating
-      user.tickets -= attraction.tickets
-      user.nausea += attraction.nausea_rating
-      user.save
       nil
     end
   end
